dff343a994a9d39d28f226a01ad8fe61fab22037
aniruddha-tech86git config --global user.email aniruddha.cloudtech@gmail.comgit config --global --listregrfjhrwgfjhrwfgjy43rtgjy453gfjy4uyfu4gf43gfi43ynyngit config --global user.name aniruddha-tech86 <aniruddha.cloudtech@gmail.com>
2026-09-12 01:30:57 +0530

    create conflict change

--- main.tf
+++ main.tf
@@ -24,4 +24,5 @@ module "vm" {
   public_ip_id        = "/subscriptions/fce5ed4b-285b-4788-8843-106cb17f385b/resourceGroups/tripura-rg/providers/Microsoft.Network/publicIPAddresses/pip-1"
 }
 
-# practicing Terraform module with Git branches
+# Master branch conflict change
+
